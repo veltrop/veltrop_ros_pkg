bfeb1912580286677914e98dad970c2c5d50fb4d
veltropt <veltropt@54fb88c8-64ab-4c58-b6db-057d306645b6>
2011-01-05 11:33:45 +0000

    send messages to enable joint groups

--- veltrop-ros-pkg/veltrobot/veltrobot_teleop/src/teleop_joy.cpp
+++ veltrop-ros-pkg/veltrobot/veltrobot_teleop/src/teleop_joy.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <sensor_msgs/JointState.h>
+#include <veltrobot_msgs/EnableJointGroup.h>
 #include <joy/Joy.h>
 
 namespace veltrobot_teleop
@@ -11,9 +12,11 @@ class VeltrobotTeleopJoy
 public:
 	VeltrobotTeleopJoy()
 	{
+  
 	  motion_pub_ = n_.advertise <std_msgs::String> ("motion_name", 1);
     joint_states_pub_ = n_.advertise<sensor_msgs::JointState>("/joint_states", 1);
     joy_sub_ = n_.subscribe <joy::Joy> ("joy", 1, &VeltrobotTeleopJoy::joyCB, this);
+    enable_joint_group_pub_ = n_.advertise<veltrobot_msgs::EnableJointGroup>("/enable_joint_group", 1);
 	}
 	
 	void spin()
@@ -25,49 +28,69 @@ private:
 	ros::NodeHandle n_;
 	ros::Publisher motion_pub_;
   ros::Publisher  joint_states_pub_;
+  ros::Publisher  enable_joint_group_pub_;
   ros::Subscriber joy_sub_;
 	
 	void joyCB(const joy::Joy::ConstPtr& joy)
   {
+    veltrobot_msgs::EnableJointGroup group;
+  
     std_msgs::String mot;
     mot.data = "";
-    static bool prev_button = false;
+    static bool prev_move = false;
     
     if (!joy->buttons[4] && !joy->buttons[5] && 
-        !joy->buttons[6] && !joy->buttons[7] && prev_button)
+        !joy->buttons[6] && !joy->buttons[7] && prev_move)
     {
       mot.data = "stand_squat";
-      prev_button = false;
+      prev_move = false;
     }
     else if (joy->buttons[7])
     {
       mot.data = "rotate_left";
-      prev_button = true;
+      prev_move = true;
     }
     else if (joy->buttons[5])
 		{
       mot.data = "rotate_right";
-      prev_button = true;
+      prev_move = true;
     }
     else if (joy->buttons[4])
 		{
       mot.data = "walk_forward";
-      prev_button = true;
+      prev_move = true;
     }
     else if (joy->buttons[6])
     {
     	mot.data = "walk_backward";
-      prev_button = true;
+      prev_move = true;
     }
+    
     else if (joy->buttons[3])
 		{
       mot.data = "init";
-      prev_button = false;
+      prev_move = false;
     }
     else if (joy->buttons[0])
     {
     	mot.data = "stand_squat";
-      prev_button = false;
+      prev_move = false;
+    }
+    
+    static bool prev_L1 = false;
+    if (joy->buttons[10] != prev_L1) // L1
+    {
+      prev_L1 = joy->buttons[10];
+      group.jointGroups.push_back("arms");
+      group.enabledStates.push_back(prev_L1);
+    }
+    
+    static bool prev_L2 = false;
+    if (joy->buttons[8] != prev_L2) // L2
+    {
+      prev_L2 = joy->buttons[8];
+      group.jointGroups.push_back("legs");
+      group.enabledStates.push_back(prev_L2);
     }
                          
     if (joy->buttons[1])
@@ -88,7 +111,10 @@ private:
 
     if (mot.data != "")
       motion_pub_.publish(mot); 
-
+      
+    if (group.jointGroups.size())
+      enable_joint_group_pub_.publish(group);
+      
   }
 };
 
