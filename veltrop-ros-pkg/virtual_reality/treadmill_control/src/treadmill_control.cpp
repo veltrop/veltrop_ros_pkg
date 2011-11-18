22e0f3efb00ba30bd7a6f4d1a74e44a1f7880b9a
veltropt <veltropt@54fb88c8-64ab-4c58-b6db-057d306645b6>
2011-11-18 08:38:11 +0000

    NAO is now functioning with treadmill based teleoperation

--- veltrop-ros-pkg/virtual_reality/treadmill_control/src/treadmill_control.cpp
+++ veltrop-ros-pkg/virtual_reality/treadmill_control/src/treadmill_control.cpp
@@ -1,6 +1,8 @@
 #include <ros/ros.h>
 #include <joy/Joy.h>
 #include <tf/transform_listener.h>
+#include <std_msgs/String.h>
+#include <geometry_msgs/Twist.h>
 
 #include <fcntl.h>
 #include <unistd.h>
@@ -11,6 +13,10 @@
 namespace treadmill
 {
 
+#define KINECT_DIST 2.7    // distance from kinect camera to center of treadmil
+#define DEAD_ZONE   0.2    // forward distance * 2 from center of treadmill to start motion.
+                           // sideways discance from center of treadmil to kill it.
+
 class TreadmillController
 {
 public:
@@ -20,6 +26,8 @@ public:
 	{
     init_tty();
     joy_sub_ = n_.subscribe <joy::Joy> ("joy", 1, &TreadmillController::joyCB, this);
+    robot_move_pub_ = n_.advertise <geometry_msgs::Twist> ("cmd_vel", 1);
+    behavior_pub_ = n_.advertise <std_msgs::String> ("motion_name", 1);
   }
 
   ~TreadmillController()
@@ -126,7 +134,8 @@ public:
     if (!setSpeed(meter_per_sec))
       return false;
     
-    return setDirection(meter_per_sec > 0 ? FORWARD : BACKWARD);
+    // seems to work more smoothly if we set direction after speed
+    return setDirection(meter_per_sec >= 0.0 ? FORWARD : BACKWARD);
   }
   
   // returns true on successful transmission 
@@ -147,17 +156,26 @@ public:
     // speed = pwm * 2.0 - 0.5
     // pwm = speed * 0.5 + 0.25
 
-    // serial port data is pwm * 10000
+    // serial port data protocol is pwm * 10000 (for precision)
   
     double pwm_scaled_double = meter_per_sec * 5000.0 + 2500.0;
     unsigned short pwm_scaled = floor(fabs(pwm_scaled_double));
 
-    //ROS_INFO_STREAM(meter_per_sec << " " << pwm_scaled_double << " " <<
-    //                pwm_scaled);
+    //ROS_INFO_STREAM(meter_per_sec << " " << pwm_scaled_double << " " << pwm_scaled);
     
-    // threshold seems like a bad workaround for the treadmills odd behavior 
-    if (pwm_scaled < 3000)
-      pwm_scaled = 3000;
+    // threshold seems like a bad workaround for the treadmills odd behavior. 
+    // if we send a zero velocity signal the treadmil will suddenly stop.
+    // but if we send a very low velocity, the treadmill will ramp down to this speed.
+    // at this very lwo velocity, the treadmil doesnt have enough power to
+    //  move me, so in effect it stops the treadmil.
+    // however, sometimes the treadmil stops responding to commands and enters
+    // some sort of safety mode.  Sending a true 0 speed can get out of this saftey mode
+    // and so does restarting the treadmill.
+    //int THRESHHOLD = 3000;
+    int THRESHHOLD = 4000;
+    //int THRESHHOLD = 5000;
+    if (pwm_scaled < THRESHHOLD)
+      pwm_scaled = THRESHHOLD;
 
     char buf[3];
     buf[0] = 1;
@@ -181,6 +199,7 @@ public:
       buf[1] = 1;
     else // go forward as fallback
       buf[1] = 2;
+      
     int w = write(tty_, buf, 2);
     if (w != 2)
     {
@@ -192,12 +211,12 @@ public:
 
   void spin()
   {
-    //ros::spin();
-
-    // TODO: fork into new node
-    ros::Rate rate(30);
-    double torso_dist_prev = 0;
+    //ros::Rate rate(30);
+    ros::Rate rate(20);
+    
     ros::Time time_prev = ros::Time::now();
+    //ros::Time prev_stand_sit = ros::Time::now();
+    bool robotStanding = false;
     while (n_.ok())
     {
       ros::spinOnce();
@@ -205,7 +224,7 @@ public:
       tf::StampedTransform torso_tf;
       try
       {
-        tf_listener_.lookupTransform("/openni_depth", "/torso", 
+        tf_listener_.lookupTransform("/openni_depth_frame", "/torso", 
                                      ros::Time(0), torso_tf);
       }
       catch (tf::TransformException ex)
@@ -218,6 +237,10 @@ public:
       //ROS_INFO_STREAM(torso_tf.getOrigin().z());
       
       double torso_dist = torso_tf.getOrigin().z();
+ /*     
+      ///////////////////////////////////////////////////////////////////////
+      //  The following is a first derivative position based method
+      double torso_dist_prev = 0;
       double torso_delta = torso_dist - torso_dist_prev;
       
       ros::Time time_now = ros::Time::now(); 
@@ -227,62 +250,127 @@ public:
       torso_dist_prev = torso_dist;
       time_prev = time_now;
       
-      //setVelocity(meters_per_sec * 10.0);
-    
-      // goal position will be 2.71 for destination based.
+      setVelocity(meters_per_sec * 10.0);
+  */  
 
-      double treadmill_velocity = -torso_dist + 2.7; // offset from goal
+      ///////////////////////////////////////////////////////////////////////
+      //  The following is a purely position based method
+
+      double treadmill_velocity = -torso_dist + KINECT_DIST; // offset from goal
       
-      // make it non linear
-      // This attempt at non-linear smooth for stopping transitions
-      // but walking at desired speed is not quite intuitive, need to alter
-      // the curve
-      if (treadmill_velocity < 0.0)
-      {
+      // linear speed factor until 0.3 cm displacement, then curved speed factor.
+      treadmill_velocity /= 0.3;
+      if (treadmill_velocity > 1.0)
         treadmill_velocity *= treadmill_velocity;
-        treadmill_velocity = -treadmill_velocity;
-      }
-      else
-      {
-        treadmill_velocity *= treadmill_velocity;
-      }
-      // 0.6, 0.5, 0.4 good for linear, 0.01, 0.005 good for non-linear
-      treadmill_velocity *= (1.0 / 0.005);
+      else if (treadmill_velocity < -1.0)
+        treadmill_velocity *= -treadmill_velocity;
+      treadmill_velocity *= 7.5; // baseline meters per second
 
-      // TODO: complete the active zone
       double torso_alignment = fabs(torso_tf.getOrigin().x());
-      double torso_height = fabs(torso_tf.getOrigin().y());
-      if (torso_alignment < 0.5/* && torso_height > 0.85*/)
+      double torso_height = torso_tf.getOrigin().y();
+      double torso_home_dist = fabs(torso_dist - KINECT_DIST);
+            
+      // handle controlling the treadmill   
+      bool robotShouldntWalk = true;  
+      bool humanStanding = torso_height >= 0.0;
+      bool withinSafeZone = torso_alignment <= DEAD_ZONE && torso_home_dist <= 0.65;
+      if (!withinSafeZone || !humanStanding)
+      {
+        stopMotor(); 
+        robotShouldntWalk = true;
+      }
+      else if (torso_home_dist >= DEAD_ZONE * 0.5)
+      {
         setVelocity(treadmill_velocity);
+        robotShouldntWalk = false;
+      }
+      else if (treadmill_velocity < 0.0)
+      {
+        setVelocity(-0.01);
+        robotShouldntWalk = true;  
+      }
       else
-        //setVelocity(0.0);
-        stopMotor();
-
+      {
+        setVelocity(0.01); 
+        robotShouldntWalk = true;
+      }    
+        
+      // next extract rotation of human for robot
+      double roll, pitch, yaw;
+      btMatrix3x3(torso_tf.getRotation()).getRPY(roll, pitch, yaw);
+ 
+      // prepare robot movement data type
+      geometry_msgs::Twist motion;
+      motion.linear.y = 0.0;
+      motion.angular.z = 0.0;
+      if (fabs(pitch) >= 0.05)
+      {
+        motion.angular.z = pitch / 1.57;
+        motion.angular.z = std::max(std::min(motion.angular.z, 1.0), -1.0);
+      }
+       
+      // handle controlling the robot          
+      if (robotStanding && !robotShouldntWalk && withinSafeZone)
+      {  
+        motion.linear.x = treadmill_velocity / 10.0;  // treat my going 10m/s as NAO's 1.0
+        motion.linear.x = std::max(std::min(motion.linear.x, 1.0), -1.0);
+        robot_move_pub_.publish(motion); 
+      }
+      else if (robotStanding && robotShouldntWalk && fabs(pitch) >= 0.2 && withinSafeZone)
+      {
+        robot_move_pub_.publish(motion); 
+      }
+      else if (robotShouldntWalk || !withinSafeZone)
+      {
+        motion.linear.x = motion.linear.y = motion.angular.z = 0.0;
+        robot_move_pub_.publish(motion); 
+      }
+      
+      //ros::Duration stand_sit_delta = ros::Time::now() - prev_stand_sit;
+      if (withinSafeZone && humanStanding && !robotStanding /*&& stand_sit_delta.toSec() > 5.0*/)
+      {
+        std_msgs::String behavior;
+        behavior.data = "get_up_and_ready";
+        behavior_pub_.publish(behavior);
+        usleep(10000000);
+        
+        robotStanding = true;
+        //prev_stand_sit = ros::Time::now();
+      }
+      else if (withinSafeZone && !humanStanding && robotStanding /*&& stand_sit_delta.toSec() > 5.0*/)
+      {
+        motion.linear.x = motion.linear.y = motion.angular.z = 0.0;
+        robot_move_pub_.publish(motion); 
+        usleep(100000);
+        
+        std_msgs::String behavior;
+        behavior.data = "sit_and_relax";
+        behavior_pub_.publish(behavior);
+        usleep(10000000);
+        
+        robotStanding = false;
+        //prev_stand_sit = ros::Time::now();
+      }
+           
       rate.sleep();
     }
   }
 
 private:
 	ros::NodeHandle n_;
-  // TODO: fork into new node
   ros::Subscriber joy_sub_;
-  // TODO: fork into new node
   tf::TransformListener tf_listener_;
-  TreadmillDirection dir_;
   int tty_;
+  ros::Publisher robot_move_pub_, behavior_pub_;
 
-	// TODO: fork into new node
   void joyCB(const joy::Joy::ConstPtr& joy)
   {
-    //joy->buttons[] 
-    
+    //joy->buttons[]    
     // range of axes is -10~10
-    // want output range -5~5mps
+    // want output range -5~5m/s
     double mps = joy->axes[0] * 0.5;
     setVelocity(mps);
   }
-
-
 };
 
 }
@@ -294,6 +382,7 @@ int main(int argc, char** argv)
 
 	this_node.spin();
   
+  /////////////////////////////////////////////////////////////////
   // a most basic test, send a speed of 500ms width pwm.
   //char buf[3];
   //buf[0] = 1;
